2e5217c001f3d28b5d20bb2e88399560e3ad40f7
TheGreatStorm <dannyjasso@yahoo.com>
2021-02-18 02:13:59 -0600

    delete requires an argument, but argument has been offsited.

--- bin/utils/argument_controller.py
+++ bin/utils/argument_controller.py
@@ -5,7 +5,7 @@ def argument_controller():
     # Plugins may have to been done manual has mods are different per game server.
     parser = argparse.ArgumentParser('Automate your Game Server\'s!')
     parser.add_argument('--create', help='Create your game server', required=False)
-    parser.add_argument('--delete', help='Delete your game server (ID,Name)', required=False)
+    parser.add_argument('--delete', help='Delete your game server (ID,Name)', required=False, action="store_true")
     parser.add_argument('--start', help='Delete your game server (ID,Name)', required=False)
     parser.add_argument('--container', help='Select container (ID,Name)', required=False)
     parser.add_argument('--config', help='Delete your game server (ID,Name)', required=False)
