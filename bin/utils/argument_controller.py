c79a564457fc72ccdad5bdecb985a71a640371b1
TheGreatStorm <dannyjasso@yahoo.com>
2021-02-18 00:14:30 -0600

    Delete a container.

--- bin/utils/argument_controller.py
+++ bin/utils/argument_controller.py
@@ -5,7 +5,7 @@ def argument_controller():
     # Plugins may have to been done manual has mods are different per game server.
     parser = argparse.ArgumentParser('Automate your Game Server\'s!')
     parser.add_argument('--create', help='Create your game server', required=False)
-    parser.add_argument('--delete', help='Delete your game server', required=False)
+    parser.add_argument('--delete', help='Delete your game server (ID,Name)', required=False)
     parser.add_argument('--update', help='Update ansible scripts, and docker image.', required=False, action='store_true')
     parser.add_argument('--install', help='Install Docker Image', required=False, action='store_true')
     parser.add_argument('--list', help='List Game Servers', required=False, action='store_true')
